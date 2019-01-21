75d8dc028fa446caf6bcde4a7d765a3585ecd898
libo <libo@usa.com>
2019-01-21 17:25:08 +0800

    modify config.php

--- src/Doccommon.php
+++ src/Doccommon.php
@@ -16,7 +16,7 @@ namespace app\api\controller;
 
 
 /**
- * @title 违章查询
+ * @title 示例title
  * @description 接口说明
  */
 class Doccommon
@@ -27,16 +27,16 @@ class Doccommon
      * @url /api/doccommon/first
      * @module 公共模块
      * 
-     * @author xzb
-     * @qqdz1 http://test.wz.c8c88.com
+     * @author libo
+     * @qqdz1 http://xxx.xxx.com
      * @qqdz2
-     * @wdsm 为了保证数据安全性，客户端与服务器端api数据交互接口需要加入sign签名，签名形式以每个接口URL后需带一个sign字段，用来校验数据合法性。Sign签名是将客户端向服务器端传递的参数，加上一个密钥，然后进行md5加密（全小写），参数按照字母a-z顺序排序。
-     * @my
-     * @cskey 33#$59*8<@
-     * @zskey
+     * @wdsm 为了保证数据安全性，数据要求加密。
+     * @my  
+     * @cskey 您的密钥请填写在这里
+     * @zskey 正式的密钥填写在这里
      * @signgs sign = MD5(url_encode(uri链接 + “?” + 参数) + 密钥KEY);
-     * @qqlj https://chewu.buyzx.cn/api/daijiao/test.html?user=xxxxx&fuck=xxxx&time=1588888888
-     * @sign md5(urlencode(“https://chewu.buyzx.cn/api/daijiao/test.html?fuck=xxxx&time=1588888888&user=xxxxx”) + 密钥key)
+     * @qqlj https://xxxx.xxx.com/api/daijiao/test.html?user=xxxxx&fuck=xxxx&time=1588888888
+     * @sign md5(urlencode(“https://xxxx.xxx.com/api/daijiao/test.html?fuck=xxxx&time=1588888888&user=xxxxx”) + 密钥key)
     计算出来的值是一个全小写32位的字符串。
      *
      * @result name:未微信授权 code:｛<br>&nbsp;&nbsp;“code”:“40301”,<br>&nbsp;&nbsp;“msg”:“尚未微信授权或微信授权已过期”,<br>&nbsp;&nbsp;“data”:{<br>&nbsp;&nbsp;&nbsp;&nbsp;“url”:“https://wx.qq.com/authorization?xxx”<br>&nbsp;&nbsp;}<br>｝ desc:（无需展示错误给用户）<br>url：用户未在微信上授权，需要重定向的链接
